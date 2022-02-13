3283ff2ef4043ca237855edaac0b090e989a71b5
Youssef <youssef.hassan@mail.com>
2022-02-13 09:42:13 +0000

    Test2

--- Test.py.py
+++ Test.py.py
@@ -5,7 +5,7 @@ Spyder Editor
 This is a temporary script file.
 """
 def testGithub():
-    message = print ('This is a test')
+    message = print ('This is a test2')
     return message
 
 message = testGithub() 
