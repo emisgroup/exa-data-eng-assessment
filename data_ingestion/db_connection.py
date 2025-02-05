c95deed3d184f257140d9f036eeeca9fdd1a5998
Sella1998 <45958089+sella1998@users.noreply.github.com>
2025-02-05 22:00:09 +0530

    Update db_connection.py

--- data_ingestion/db_connection.py
+++ data_ingestion/db_connection.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 
-print('run this command to get the postgres IP `docker inspect postgres_container | grep "IPAddress"`')
+print('run this command to get the postgres IP `docker inspect postgres_container_emis | grep "IPAddress"`')
 ip = input()
 if ip:
     db_conn = create_engine(f"postgresql+psycopg2://admin1:password@{ip}:5432/fhir")
